fd6570659920b13c22d5727e888a786fe4c3314e
Batman <146239796+JoeGiwa@users.noreply.github.com>
2025-05-05 05:27:06 -0500

    Add model evaluation function

--- model.py
+++ model.py
@@ -1,9 +1,14 @@
- import numpy as np
+import numpy as np
 import pandas as pd
 def train_model():
- print ("Training ML model...")
- # model training would go here
+  print ("Training ML model...")
+  # model training would go here
 
 if __name__ == "__main__":
- print("MLOps Project Initialized")
- train_model()
+   print("MLOps Project Initialized")
+   train_model()
+
+def evaluate_model():
+ print("Evaluating model performance...")
+ # Evalaution code would go here
+ return {"accuracy" : 0.95}
