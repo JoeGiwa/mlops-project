afa5d8e31cb5947edc4faa20757a1a22a73c232f
jgiwa <jgiwa@depaul.edu>
2025-05-05 05:05:27 -0500

    Intial commit for MLOps project

---
+++ model.py
@@ -0,0 +1,9 @@
+ import numpy as np
+import pandas as pd
+def train_model():
+ print ("Training ML model...")
+ # model training would go here
+
+if __name__ == "__main__":
+ print("MLOps Project Initialized")
+ train_model()
